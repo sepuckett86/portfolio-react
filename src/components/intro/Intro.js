ecbb95d033c77a705c92d88aa6338709eb1b7feb
sepuckett86 <sepuckett86@gmail.com>
2019-10-01 13:06:47 -0700

    update content of intro

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -2,41 +2,22 @@ import React, { useState } from 'react';
 import styles from './Intro.css';
 import PropTypes from 'prop-types';
 
-function Intro({ introStatements, strengths, weaknesses }) {
+function Intro({ introStatements }) {
   const [index, setIndex] = useState(0);
   const statement = introStatements[index];
 
-  const strengthItems = strengths.map(strength => (
-    <li key={strength}>{strength}</li>
-  ));
-
-  const weaknessItems = weaknesses.map(weakness => (
-    <li key={weakness}>{weakness}</li>
-  ));
-
   return (
     <section className={styles.Intro}>
-      <h1>Welcome to my site!</h1>
-      <h2>Intro</h2>
-      <h3>About me:</h3>
+      <h1>Susan Puckett</h1>
+      <h2>Software Developer</h2>
       <p>{statement}</p>
       <button onClick={() => index < introStatements.length - 1 ? setIndex(index + 1) : setIndex(0)}>Next</button>
-      <h3>My strengths</h3>
-      <ul>
-        {strengthItems}
-      </ul>
-      <h3>My weaknesses</h3>
-      <ul>
-        {weaknessItems}
-      </ul>
     </section>
   );
 }
 
 Intro.propTypes = {
-  introStatements: PropTypes.arrayOf(PropTypes.string).isRequired,
-  strengths: PropTypes.arrayOf(PropTypes.string).isRequired,
-  weaknesses: PropTypes.arrayOf(PropTypes.string).isRequired,
+  introStatements: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default Intro;
--- src/containers/IntroContainer.js
+++ src/containers/IntroContainer.js
@@ -5,15 +5,17 @@ import Media from '../components/intro/Media';
 class IntroContainer extends Component {
   state = {
     introStatements: [
-      'With a background in science and a passion for building things with code, I aim to combine data analysis with app development.',
-      'An introvert who also loves to contribute to a team, I aim to find that sweet spot of balance of socialization and focused alone time.',
-      'Here is where a witty intro statement should go that accentuates my personality. Hi?',
+      'With a background in science and a passion for building things with code, I want to combine my love of data with app development.',
+      'An introvert who also loves to participate in a team, I aim to find that sweet spot of collaboration and focused alone-time.',
       'Not a fan of the idea of networking, but am a fan of getting to know tech-passionate people.',
-      'Fun fact: according to the Gallup Clifton Strengths test, my top strengths are adaptability, learner, intellection, input, and responsibility.',
+      'According to the Gallup Clifton Strengths test, my top strengths are adaptability, learner, intellection, input, and responsibility.',
       'On the fence about the trailing comma.',
       'One of my life goals is to play in clarinet in a community band until I die.',
-      'Over 1.1k Github contributions in the past year as of Sept 2019.',
-      'Over 800 hours of full-stack JavaScript app development in a cohort of 19 people.'
+      'Over 1.1k Github contributions in the past year as of Sept 2019. Please give me a job already. ;)',
+      'Over 800 hours of full-stack JavaScript app development experience in a cohort of 19 wonderful people.',
+      'My top bucket list item: Visit a sloth sanctuary in Costa Rica.',
+      'Liked sloths before Kristen Bell made them popular.',
+      'Here is where a witty intro statement should go that accentuates my personality. Hi?'
     ],
     strengths: [
       'detail-oriented',
